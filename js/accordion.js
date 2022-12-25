2ff2d41e280d0dae933107b2b9b3758c00f5cf4f
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2022-12-25 10:18:22 -0300

    criado areaListProdutos / accordion.js ajustes

--- js/accordion.js
+++ js/accordion.js
@@ -3,16 +3,22 @@ export const accordion = () => {
   const leiaMais = document.querySelector(".js-accordion-leiaMais");
   const reduzirTexto = document.querySelector(".js-accordion-reduzirTexto");
 
+  const refScroll = document.querySelector(".js-accordion-ref");
+
   const accordionAction = () => {
     accordionElement.classList.toggle("ativo");
 
+    verifyHeight();
+
+    changeStatusLeiaMais();
+  };
+
+  const verifyHeight = () => {
     if (accordionElement.classList.contains("ativo")) {
       accordionElement.style.height = accordionElement.scrollHeight + 10 + "px";
     } else {
       accordionElement.style.height = 0;
     }
-
-    changeStatusLeiaMais();
   };
 
   const changeStatusLeiaMais = () => {
@@ -21,7 +27,26 @@ export const accordion = () => {
       : (leiaMais.innerText = "Leia mais >");
   };
 
+  function Position(obj) {
+    var currenttop = 0;
+    if (obj.offsetParent) {
+      do {
+        currenttop += obj.offsetTop;
+      } while ((obj = obj.offsetParent));
+      return [currenttop];
+    }
+  }
+
+  const scrollToElement = () => {
+    window.scroll(
+      0,
+      Position(document.getElementById("js-accordion-ref-scroll"))
+    );
+  };
+
   const handleReduzirText = () => {
+    /*leiaMais.scrollIntoView({ behavior: "smooth", }, true);*/
+    scrollToElement;
     accordionAction();
     changeStatusLeiaMais();
   };
